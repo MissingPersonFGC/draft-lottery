d34e1d4567c6a10026c95da533a704a3e5df5c50
Corey Lanier <iam@coreylanier.com>
2018-06-27 14:59:32 -0400

    generating player pool

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,3 +1,36 @@
-const ohHey = "Hello World";
+const app = {};
 
-console.log(ohHey);
+const playerPool = [];
+
+const $playerAdd = $(`#player-add`);
+const $newPlayer = $(`#new-player`);
+const $playerPoolList = $(`#player-pool ul`);
+
+app.init = () => {
+    app.getNewPlayer();
+}
+
+app.getNewPlayer = () => {
+    $playerAdd.on(`submit`, (e) => {
+        e.preventDefault();
+        const newPlayer = $newPlayer.val();
+        app.pushNewPlayer(newPlayer);
+    });
+}
+
+app.pushNewPlayer = (player) => {
+    playerPool.push(player);
+    $newPlayer.val(``);
+    app.populatePlayerPool();
+}
+
+app.populatePlayerPool = () => {
+    $playerPoolList.empty();
+    playerPool.forEach((player) => {
+        $playerPoolList.append(`
+            <li>${player}</li>
+        `);
+    });
+}
+
+$(app.init);
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,36 @@
 "use strict";
 
-var ohHey = "Hello World";
+var app = {};
 
-console.log(ohHey);
+var playerPool = [];
+
+var $playerAdd = $("#player-add");
+var $newPlayer = $("#new-player");
+var $playerPoolList = $("#player-pool ul");
+
+app.init = function () {
+    app.getNewPlayer();
+};
+
+app.getNewPlayer = function () {
+    $playerAdd.on("submit", function (e) {
+        e.preventDefault();
+        var newPlayer = $newPlayer.val();
+        app.pushNewPlayer(newPlayer);
+    });
+};
+
+app.pushNewPlayer = function (player) {
+    playerPool.push(player);
+    $newPlayer.val("");
+    app.populatePlayerPool();
+};
+
+app.populatePlayerPool = function () {
+    $playerPoolList.empty();
+    playerPool.forEach(function (player) {
+        $playerPoolList.append("\n            <li>" + player + "</li>\n        ");
+    });
+};
+
+$(app.init);
